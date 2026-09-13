11f8ee4693a5382a1ac85eb0a959f88e5bbd8fe9
minhazrabbi <rabbilegend7@gmail.com>
2026-09-13 17:25:37 +0600

    added techonology data model

---
+++ src/types/technology.ts
@@ -0,0 +1,10 @@
+export type Technology = {
+  id: number;
+  name: string;
+  category: string;
+  skillLevel: string;
+  description: string;
+  rating: number;
+  badges: string;
+  logo: string;
+};
